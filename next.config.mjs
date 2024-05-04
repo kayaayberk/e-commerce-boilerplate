e2079d6e724671ee2808fd437a0ef20cdde331ee
Ayberk Kaya <kayaayberk98@gmail.com>
2024-05-04 21:52:23 +0200

    Initial commit from Create Next App

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withVercelToolbar from '@vercel/toolbar/plugins/next'
+import withBundleAnalyzer from '@next/bundle-analyzer'
+import withPlugins from 'next-compose-plugins'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = withPlugins([[withVercelToolbar(), withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })]], {
+  reactStrictMode: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    imageSizes: [256, 384],
+    deviceSizes: [320, 500, 750, 1080, 1200],
+    minimumCacheTTL: 31_556_926,
+    formats: ['image/webp'],
+    // remotePatterns: [{}],
+  },
+})
+
+export default nextConfig
