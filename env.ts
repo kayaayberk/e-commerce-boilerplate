06a439438ec9323adf9b7e6599a0e0d375484a33
Ayberk Kaya <kayaayberk98@gmail.com>
2024-05-05 21:36:53 +0200

    chore: t3-env

---
+++ env.ts
@@ -0,0 +1,19 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  // skipValidation: process.env.NODE_ENV !== 'production' || process.env.SKIP_ENV_VALIDATION === 'true',
+  server: {
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN: z.string(),
+    SHOPIFY_ADMIN_ACCESS_TOKEN: z.string().optional(),
+    SHOPIFY_APP_API_SECRET_KEY: z.string().optional(),
+    SHOPIFY_STORE_DOMAIN: z.string(),
+  },
+  client: {},
+  runtimeEnv: {
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+    SHOPIFY_ADMIN_ACCESS_TOKEN: process.env.SHOPIFY_ADMIN_ACCESS_TOKEN,
+    SHOPIFY_APP_API_SECRET_KEY: process.env.SHOPIFY_APP_API_SECRET_KEY,
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+  },
+})
