8b93e6cef082cb0e51252169d3253dc8090c6e95
Ayberk Kaya <kayaayberk98@gmail.com>
2024-05-12 23:25:38 +0200

    chore: Update next.config.mjs with image remotePatterns for Shopify CDN

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,32 @@
 import withVercelToolbar from '@vercel/toolbar/plugins/next'
 import withBundleAnalyzer from '@next/bundle-analyzer'
 import withPlugins from 'next-compose-plugins'
+import { hostname } from 'os'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPlugins([[withVercelToolbar(), withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })]], {
-  reactStrictMode: true,
-  logging: {
-    fetches: {
-      fullUrl: true,
+const nextConfig = withPlugins(
+  [[withVercelToolbar(), withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })]],
+  {
+    reactStrictMode: true,
+    logging: {
+      fetches: {
+        fullUrl: true,
+      },
     },
-  },
-  images: {
-    imageSizes: [256, 384],
-    deviceSizes: [320, 500, 750, 1080, 1200],
-    minimumCacheTTL: 31_556_926,
-    formats: ['image/webp'],
-    // remotePatterns: [{}],
-  },
-})
+    images: {
+      imageSizes: [256, 384],
+      deviceSizes: [320, 500, 750, 1080, 1200],
+      minimumCacheTTL: 31_556_926,
+      formats: ['image/webp'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.shopify.com',
+          port: '',
+        },
+      ],
+    },
+  }
+)
 
 export default nextConfig
