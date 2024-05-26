88cd32414b7b00fc3f57717999765555cc13002e
Ayberk Kaya <kayaayberk98@gmail.com>
2024-05-27 01:04:20 +0200

    fix: no-wrap whitespace

--- views/ProductListing/Sorter.tsx
+++ views/ProductListing/Sorter.tsx
@@ -1,23 +1,28 @@
-"use client"
+'use client'
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { parseAsStringEnum, useQueryState } from "nuqs"
-import { Icons } from "@/components/Icons/Icons"
+import { parseAsStringEnum, useQueryState } from 'nuqs'
+import { Icons } from '@/components/Icons/Icons'
+import {
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenu,
+} from '@/components/ui/dropdown-menu'
 
 export enum Sorting {
-  PRICE_DESC = "minPrice:desc",
-  PRICE_ASC = "minPrice:asc",
-  DATE_ASC = "updatedAtTimestamp:asc",
-  DATE_DESC = "updatedAtTimestamp:desc",
-  RELEVANCY = "",
+  PRICE_DESC = 'minPrice:desc',
+  PRICE_ASC = 'minPrice:asc',
+  DATE_ASC = 'updatedAtTimestamp:asc',
+  DATE_DESC = 'updatedAtTimestamp:desc',
+  RELEVANCY = '',
 }
 
 const LABELS = {
-  [Sorting.PRICE_DESC]: "Price: High to Low",
-  [Sorting.PRICE_ASC]: "Price: Low to High",
-  [Sorting.DATE_ASC]: "Newest",
-  [Sorting.DATE_DESC]: "Oldest",
-  [Sorting.RELEVANCY]: "Relevancy",
+  [Sorting.PRICE_DESC]: 'Price: High to Low',
+  [Sorting.PRICE_ASC]: 'Price: Low to High',
+  [Sorting.DATE_ASC]: 'Newest',
+  [Sorting.DATE_DESC]: 'Oldest',
+  [Sorting.RELEVANCY]: 'Relevancy',
 }
 
 interface SorterProps {
@@ -25,26 +30,29 @@ interface SorterProps {
 }
 
 export function Sorter({ className }: SorterProps) {
-  const [sortBy, setSortBy] = useQueryState("sortBy", {
+  const [sortBy, setSortBy] = useQueryState('sortBy', {
     ...parseAsStringEnum<Sorting>(Object.values(Sorting)).withDefault(Sorting.RELEVANCY),
     shallow: false,
-    history: "push",
+    history: 'push',
   })
 
   return (
     <div className={className}>
       <DropdownMenu>
         <DropdownMenuTrigger aria-expanded={undefined} asChild>
-          <div className="flex cursor-pointer flex-wrap items-center justify-center gap-0.5 text-[15px] text-black">
-            Sort by: <span className="ml-0.5 text-slate-700 underline">{LABELS[sortBy]}</span>
-            <Icons.ChevronDown />
+          <div className='flex cursor-pointer items-center justify-center gap-0.5 whitespace-nowrap text-[15px] text-black'>
+            Sort by: <span className='ml-0.5 text-slate-700'>{LABELS[sortBy]}</span>
+            <Icons.ChevronDown className='size-4' />
           </div>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-[240px] rounded-b-md bg-white px-0 text-neutral-500 shadow-lg" align="end">
+        <DropdownMenuContent
+          className='w-[240px] rounded-b-md bg-white px-0 text-neutral-500 shadow-lg'
+          align='end'
+        >
           {Object.entries(LABELS).map(([key, label]) => (
             <DropdownMenuItem
               key={label}
-              className="cursor-pointer border-b border-neutral-200 py-2 last:border-b-0 hover:bg-neutral-50 focus:bg-neutral-50 active:bg-neutral-50"
+              className='cursor-pointer border-b border-neutral-200 py-2 last:border-b-0 hover:bg-neutral-50 focus:bg-neutral-50 active:bg-neutral-50'
               onClick={() => setSortBy(key as Sorting)}
             >
               {label}
