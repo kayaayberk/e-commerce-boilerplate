140be2eadecae85e58f1f675c11d1bed6237ce77
Ayberk Kaya <kayaayberk98@gmail.com>
2024-05-30 23:56:15 +0200

    chore: add cart icon

--- components/Icons/Icons.tsx
+++ components/Icons/Icons.tsx
@@ -1,10 +1,11 @@
 // prettier-ignore
-import { ArrowLeft, ChevronDown, CircleUserRound, Facebook, Heart, Instagram, Link, Linkedin, LoaderCircle, LucideProps, PencilLine, Search, Slash, SlidersHorizontal, Twitter, X, Youtube } from 'lucide-react'
+import { ArrowLeft, ChevronDown, CircleUserRound, Facebook, Heart, Instagram, Link, Linkedin, LoaderCircle, LucideProps, PencilLine, Search, ShoppingCart, Slash, SlidersHorizontal, Twitter, X, Youtube } from 'lucide-react'
 import { Logo } from '../Logo/Logo'
 // prettier-ignore
 export const Icons = {
   Filters:     (props: LucideProps) => <SlidersHorizontal {...props} />,
   User:        (props: LucideProps) => <CircleUserRound {...props} />,
+  Cart:        (props: LucideProps) => <ShoppingCart {...props} />,
   Spinner:     (props: LucideProps) => <LoaderCircle {...props} />,
   ChevronDown: (props: LucideProps) => <ChevronDown {...props} />,
   Edit:        (props: LucideProps) => <PencilLine {...props} />,
